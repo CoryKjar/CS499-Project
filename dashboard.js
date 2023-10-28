a1ce91050d4dc77df39a251d0113849958997daa
Cory Kjar <ckjar112@gmail.com>
2023-10-27 23:47:10 -0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,67 @@
 var df; // To store the CSV data
 
 // Function to load CSV data and parse it
+// Function to load CSV data and parse it
+function loadDataAndParseCSV() {
+    Papa.parse("data.csv", {
+        header: true,
+        dynamicTyping: true,
+        download: true,
+        complete: function(results) {
+            // Store the parsed data in the df variable
+            df = results.data;
+            populateDropdowns();
+        }
+    });
+}
+
+// Function to populate state and variable dropdowns
+function populateDropdowns() {
+    var stateDropdown = document.getElementById("state-dropdown");
+    var yVariableDropdown = document.getElementById("y-variable-dropdown");
+    var barChartVariableDropdown = document.getElementById("bar-chart-variable-dropdown");
+
+    // Clear existing options
+    stateDropdown.innerHTML = "";
+    yVariableDropdown.innerHTML = "";
+    barChartVariableDropdown.innerHTML = "";
+
+    // Get unique states
+    var uniqueStates = [...new Set(df.map(row => row.State))];
+
+    // Add states to the state dropdown
+    uniqueStates.forEach(function(state) {
+        var option = document.createElement("option");
+        option.value = state;
+        option.text = state;
+        stateDropdown.appendChild(option);
+    });
+
+    // Get variable names
+    var variableNames = Object.keys(df[0]);
+    var filteredVariables = variableNames.filter(name => name !== "Quarter" && name !== "State");
+
+    // Add variables to the line plot variable dropdown
+    filteredVariables.forEach(function(variable) {
+        var option = document.createElement("option");
+        option.value = variable;
+        option.text = variable;
+        yVariableDropdown.appendChild(option);
+    });
+
+    // Add variables to the bar chart variable dropdown
+    filteredVariables.forEach(function(variable) {
+        var option = document.createElement("option");
+        option.value = variable;
+        option.text = variable;
+        barChartVariableDropdown.appendChild(option);
+    });
+
+    // Initialize the plot with default values
+    updateLinePlot();
+    updateBarChart();
+}
+
 function loadDataAndParseCSV() {
     Papa.parse("data.csv", {
         header: true,
