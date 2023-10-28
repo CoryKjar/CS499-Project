3d103c13e61a08686c33e02562c76ba2080f8e9a
Cory Kjar <ckjar112@gmail.com>
2023-10-27 21:44:41 -0500

    1027

--- dashboard.js
+++ dashboard.js
@@ -1,19 +1,72 @@
-// interactive_lineplot.js
+// Function to load CSV data and parse it
+function loadDataAndParseCSV() {
+    Papa.parse("data.csv", {
+        header: true,
+        dynamicTyping: true,
+        download: true,
+        complete: function(results) {
+            // Store the parsed data in the df variable
+            df = results.data;
+            populateDropdowns();
+        }
+    });
+}
+
+// Function to populate state and variable dropdowns
+function populateDropdowns() {
+    var stateDropdown = document.getElementById("state-dropdown");
+    var yVariableDropdown = document.getElementById("y-variable-dropdown");
+
+    // Clear existing options
+    stateDropdown.innerHTML = "";
+    yVariableDropdown.innerHTML = "";
+
+    // Get unique states
+    var uniqueStates = [...new Set(df.map(row => row.State))];
+
+    // Add states to the state dropdown
+    uniqueStates.forEach(function(state) {
+        var option = document.createElement("option");
+        option.value = state;
+        option.text = state;
+        stateDropdown.appendChild(option);
+    });
+
+    // Get variable names
+    var variableNames = Object.keys(df[0]);
+    var filteredVariables = variableNames.filter(name => name !== "Quarter" && name !== "State");
+
+    // Add variables to the variable dropdown
+    filteredVariables.forEach(function(variable) {
+        var option = document.createElement("option");
+        option.value = variable;
+        option.text = variable;
+        yVariableDropdown.appendChild(option);
+    });
+
+    // Initialize the plot with default values
+    updateLinePlot();
+}
 
 // Function to update the line plot
-function updateLinePlot(state, y_variable) {
+function updateLinePlot() {
+    var stateDropdown = document.getElementById("state-dropdown");
+    var yVariableDropdown = document.getElementById("y-variable-dropdown");
+    var selectedState = stateDropdown.value;
+    var selectedVariable = yVariableDropdown.value;
+
     // Filter the data for the selected state
-    var df_state = df.filter(function (row) {
-        return row.State === state;
+    var df_state = df.filter(function(row) {
+        return row.State === selectedState;
     });
 
     // Extract x and y data
-    var x = df_state.map(function (row) {
+    var x = df_state.map(function(row) {
         return row.Quarter;
     });
 
-    var y = df_state.map(function (row) {
-        return row[y_variable];
+    var y = df_state.map(function(row) {
+        return row[selectedVariable];
     });
 
     // Create a Plotly line plot
@@ -21,42 +74,24 @@ function updateLinePlot(state, y_variable) {
         x: x,
         y: y,
         mode: 'lines',
-        name: y_variable,
+        name: selectedVariable,
     };
 
     var data = [trace];
 
     var layout = {
-        title: `${y_variable} Over Time in ${state}`,
+        title: `${selectedVariable} Over Time in ${selectedState}`,
         xaxis: {
             title: 'Quarter',
             tickangle: -45,
         },
         yaxis: {
-            title: y_variable,
+            title: selectedVariable,
         },
     };
 
     Plotly.newPlot('line-plot', data, layout);
 }
 
-// Event listener for state and y_variable dropdowns
-var stateDropdown = document.getElementById('state-dropdown');
-var yVariableDropdown = document.getElementById('y-variable-dropdown');
-
-stateDropdown.addEventListener('change', function () {
-    var state = stateDropdown.value;
-    var y_variable = yVariableDropdown.value;
-    updateLinePlot(state, y_variable);
-});
-
-yVariableDropdown.addEventListener('change', function () {
-    var state = stateDropdown.value;
-    var y_variable = yVariableDropdown.value;
-    updateLinePlot(state, y_variable);
-});
-
-// Initial update of the plot
-var initialState = df[0].State; // You can set an initial state
-var initialYVariable = df.columns[2]; // You can set an initial y variable
-updateLinePlot(initialState, initialYVariable);
+// Load CSV data and parse it when the page loads
+loadDataAndParseCSV();
